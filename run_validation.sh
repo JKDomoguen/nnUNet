3b5d1e71051e9b4ae26988ab6efdc7e53aea8750
Jansen Domoguen <jansenkeith.domoguen@gmail.com>
2022-09-16 21:46:11 +0800

    Adding Training Script

---
+++ run_validation.sh
@@ -0,0 +1,14 @@
+nnUNet_train 3d_fullres nnUNetTrainerV2 Task505_NPC 2 --npz
+nnUNet_train 3d_fullres nnUNetTrainerV2 Task505_NPC 3 --npz
+
+nnUNet_train 3d_lowres nnUNetTrainerV2 Task505_NPC 0 --npz
+nnUNet_train 3d_lowres nnUNetTrainerV2 Task505_NPC 1 --npz
+nnUNet_train 3d_lowres nnUNetTrainerV2 Task505_NPC 2 --npz
+nnUNet_train 3d_lowres nnUNetTrainerV2 Task505_NPC 3 --npz
+
+nnUNet_train 3d_cascade_fullres nnUNetTrainerV2 Task505_NPC 0 --npz
+nnUNet_train 3d_cascade_fullres nnUNetTrainerV2 Task505_NPC 1 --npz
+nnUNet_train 3d_cascade_fullres nnUNetTrainerV2 Task505_NPC 2 --npz
+nnUNet_train 3d_cascade_fullres nnUNetTrainerV2 Task505_NPC 3 --npz
+
+
