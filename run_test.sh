a8d72c467f1cbfb072db34132eb8b454b8a43f76
Jansen Domoguen <jansenkeith.domoguen@gmail.com>
2022-09-21 13:33:13 +0800

    Uploading Training Script

---
+++ run_test.sh
@@ -0,0 +1,19 @@
+nnUNet_train 3d_fullres nnUNetTrainerV2 Task520_NPC all --npz
+nnUNet_train 3d_fullres nnUNetTrainerV2 Task521_NPC all --npz
+nnUNet_train 3d_fullres nnUNetTrainerV2 Task522_NPC all --npz
+nnUNet_train 3d_fullres nnUNetTrainerV2 Task523_NPC all --npz
+nnUNet_train 3d_fullres nnUNetTrainerV2 Task524_NPC all --npz
+nnUNet_train 3d_fullres nnUNetTrainerV2 Task525_NPC all --npz
+nnUNet_train 3d_fullres nnUNetTrainerV2 Task526_NPC all --npz
+
+# nnUNet_train 3d_lowres nnUNetTrainerV2 Task505_NPC 0 --npz
+# nnUNet_train 3d_lowres nnUNetTrainerV2 Task505_NPC 1 --npz
+# nnUNet_train 3d_lowres nnUNetTrainerV2 Task505_NPC 2 --npz
+# nnUNet_train 3d_lowres nnUNetTrainerV2 Task505_NPC 3 --npz
+
+# nnUNet_train 3d_cascade_fullres nnUNetTrainerV2 Task505_NPC 0 --npz
+# nnUNet_train 3d_cascade_fullres nnUNetTrainerV2 Task505_NPC 1 --npz
+# nnUNet_train 3d_cascade_fullres nnUNetTrainerV2 Task505_NPC 2 --npz
+# nnUNet_train 3d_cascade_fullres nnUNetTrainerV2 Task505_NPC 3 --npz
+
+
