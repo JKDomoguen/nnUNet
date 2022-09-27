052f286a84a8902df6924e991154606ce07adcd7
Jansen Domoguen <jansenkeith.domoguen@gmail.com>
2022-09-27 23:49:04 +0800

    Re-running Training

--- run_train.sh
+++ run_train.sh
@@ -1,8 +1,8 @@
-nnUNet_train 3d_fullres nnUNetTrainerV2 Task520_NPC all --npz
-nnUNet_train 3d_fullres nnUNetTrainerV2 Task521_NPC all --npz
-nnUNet_train 3d_fullres nnUNetTrainerV2 Task522_NPC all --npz
-nnUNet_train 3d_fullres nnUNetTrainerV2 Task523_NPC all --npz
-nnUNet_train 3d_fullres nnUNetTrainerV2 Task524_NPC all --npz
+# nnUNet_train 3d_fullres nnUNetTrainerV2 Task520_NPC all --npz
+# nnUNet_train 3d_fullres nnUNetTrainerV2 Task521_NPC all --npz
+# nnUNet_train 3d_fullres nnUNetTrainerV2 Task522_NPC all --npz
+# nnUNet_train 3d_fullres nnUNetTrainerV2 Task523_NPC all --npz
+# nnUNet_train 3d_fullres nnUNetTrainerV2 Task524_NPC all --npz
 nnUNet_train 3d_fullres nnUNetTrainerV2 Task525_NPC all --npz
 nnUNet_train 3d_fullres nnUNetTrainerV2 Task526_NPC all --npz
 
