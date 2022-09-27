88a7aeff3464d2c1e7a42c05131e836a7647ed3e
Jansen Domoguen <jansenkeith.domoguen@gmail.com>
2022-09-27 22:54:13 +0800

    Adding

--- eval_test_output.py
+++ eval_test_output.py
@@ -88,6 +88,7 @@ def binary_dice(s, g, resize = False):
 
 SLICE = 16
 CLASS_NUM = 2
+dice_ave = []
 if __name__ == "__main__":
     fold = str(sys.argv[1])
     output_prediction_dir = f"/media/disk1/jansen/code_rad/Dataset_Rad3/iteration_{fold}/gtv_test/uncut_scale_unproc/nnunet_output"
@@ -103,29 +104,30 @@ if __name__ == "__main__":
         img_nifti_out, _ = load_origin_nifty_volume_as_array(nifti_out_path)
         img_nifti_gt,_ = load_origin_nifty_volume_as_array(nifti_gt_path)    
         dice = binary_dice(img_nifti_out,img_nifti_gt)
-        print(nifti_out_file,nifti_out_file.replace('image','mask').strip('.gz'),dice)
-        continue
-        
+        dice_ave.append(dice)
+        print(nifti_out_file,nifti_out_file.replace('image','mask').strip('.gz'),dice)        
+        print('Done Next Image\n\n')
+    dice_ave = round(sum(dice_ave)/len(dice_ave),3)
+    print(f"Average Value:{dice_ave}")
         # print(img_nifti_out.shape,img_nifti_gt.shape)
         # print(np.unique(img_nifti_out),np.unique(img_nifti_gt))
-        soft_out_seq = []
-        soft_label_seq = []
-        for idx in range((len(img_nifti_gt)//SLICE + 1)):
-            img_nifti_out_slice = img_nifti_out[idx*SLICE:(idx+1)*SLICE]
-            img_nifti_gt_slice = img_nifti_gt[idx*SLICE:(idx+1)*SLICE]
-            # if len(np.unique(img_nifti_gt_slice)) != CLASS_NUM:
-            #     continue 
-            print(img_nifti_out_slice.shape,img_nifti_gt_slice.shape)
-            soft_out_seq.append(get_soft_label(img_nifti_out_slice,CLASS_NUM))
-            soft_label_seq.append(get_soft_label(img_nifti_gt_slice,CLASS_NUM))
+        # soft_out_seq = []
+        # soft_label_seq = []
+        # for idx in range((len(img_nifti_gt)//SLICE + 1)):
+        #     img_nifti_out_slice = img_nifti_out[idx*SLICE:(idx+1)*SLICE]
+        #     img_nifti_gt_slice = img_nifti_gt[idx*SLICE:(idx+1)*SLICE]
+        #     # if len(np.unique(img_nifti_gt_slice)) != CLASS_NUM:
+        #     #     continue 
+        #     print(img_nifti_out_slice.shape,img_nifti_gt_slice.shape)
+        #     soft_out_seq.append(get_soft_label(img_nifti_out_slice,CLASS_NUM))
+        #     soft_label_seq.append(get_soft_label(img_nifti_gt_slice,CLASS_NUM))
         
-        soft_label_seq = np.concatenate(soft_label_seq,axis=2)
-        soft_out_seq = np.concatenate(soft_out_seq,axis=2)
+        # soft_label_seq = np.concatenate(soft_label_seq,axis=2)
+        # soft_out_seq = np.concatenate(soft_out_seq,axis=2)
 
-        soft_label_seq = np.expand_dims(soft_label_seq,axis=0)
-        soft_out_seq = np.expand_dims(soft_out_seq,axis=0)
+        # soft_label_seq = np.expand_dims(soft_label_seq,axis=0)
+        # soft_out_seq = np.expand_dims(soft_out_seq,axis=0)
         
-        gtv_dice = get_classwise_dice(soft_out_seq,soft_label_seq)
-        for c in range(CLASS_NUM):
-            print('class_{}_dice,Test_dice_value:{}'.format(c,gtv_dice[c]))
-        print('Done Next Image\n\n')
+        # gtv_dice = get_classwise_dice(soft_out_seq,soft_label_seq)
+        # for c in range(CLASS_NUM):
+        #     print('class_{}_dice,Test_dice_value:{}'.format(c,gtv_dice[c]))
