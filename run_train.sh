95e7b3693289b2039dac935cafb3d755f282e3d1
Jansen Domoguen <jansenkeith.domoguen@gmail.com>
2022-09-29 00:05:15 +0800

    3d-cascade Training

--- run_train.sh
+++ run_train.sh
@@ -3,19 +3,20 @@
 # nnUNet_train 3d_fullres nnUNetTrainerV2 Task522_NPC all --npz
 # nnUNet_train 3d_fullres nnUNetTrainerV2 Task523_NPC all --npz
 # nnUNet_train 3d_fullres nnUNetTrainerV2 Task524_NPC all --npz
-nnUNet_plan_and_preprocess -t 525 --verify_dataset_integrity
-nnUNet_train 3d_fullres nnUNetTrainerV2 Task525_NPC all --npz
-nnUNet_plan_and_preprocess -t 526 --verify_dataset_integrity
-nnUNet_train 3d_fullres nnUNetTrainerV2 Task526_NPC all --npz
+# nnUNet_plan_and_preprocess -t 525 --verify_dataset_integrity
+# nnUNet_train 3d_fullres nnUNetTrainerV2 Task525_NPC all --npz
+# nnUNet_plan_and_preprocess -t 526 --verify_dataset_integrity
+# nnUNet_train 3d_fullres nnUNetTrainerV2 Task526_NPC all --npz
 
 # nnUNet_train 3d_lowres nnUNetTrainerV2 Task505_NPC 0 --npz
 # nnUNet_train 3d_lowres nnUNetTrainerV2 Task505_NPC 1 --npz
 # nnUNet_train 3d_lowres nnUNetTrainerV2 Task505_NPC 2 --npz
 # nnUNet_train 3d_lowres nnUNetTrainerV2 Task505_NPC 3 --npz
 
-# nnUNet_train 3d_cascade_fullres nnUNetTrainerV2 Task505_NPC 0 --npz
-# nnUNet_train 3d_cascade_fullres nnUNetTrainerV2 Task505_NPC 1 --npz
-# nnUNet_train 3d_cascade_fullres nnUNetTrainerV2 Task505_NPC 2 --npz
-# nnUNet_train 3d_cascade_fullres nnUNetTrainerV2 Task505_NPC 3 --npz
-
-
+nnUNet_train 3d_cascade_fullres nnUNetTrainerV2 Task520_NPC all --npz
+nnUNet_train 3d_cascade_fullres nnUNetTrainerV2 Task521_NPC all --npz
+nnUNet_train 3d_cascade_fullres nnUNetTrainerV2 Task522_NPC all --npz
+nnUNet_train 3d_cascade_fullres nnUNetTrainerV2 Task523_NPC all --npz
+nnUNet_train 3d_cascade_fullres nnUNetTrainerV2 Task524_NPC all --npz
+nnUNet_train 3d_cascade_fullres nnUNetTrainerV2 Task525_NPC all --npz
+nnUNet_train 3d_cascade_fullres nnUNetTrainerV2 Task526_NPC all --npz
