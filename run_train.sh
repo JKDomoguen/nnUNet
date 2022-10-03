01f83acd6e41d1b969a8d794f8a203bde05fff9a
Jansen Domoguen <jansenkeith.domoguen@gmail.com>
2022-10-03 08:41:52 +0800

    Adding NNUNet full cascade

--- run_train.sh
+++ run_train.sh
@@ -8,18 +8,18 @@
 # nnUNet_plan_and_preprocess -t 526 --verify_dataset_integrity
 # nnUNet_train 3d_fullres nnUNetTrainerV2 Task526_NPC all --npz
 
-nnUNet_train 3d_lowres nnUNetTrainerV2 Task520_NPC all --npz
-nnUNet_train 3d_lowres nnUNetTrainerV2 Task521_NPC all --npz
-nnUNet_train 3d_lowres nnUNetTrainerV2 Task522_NPC all --npz
-nnUNet_train 3d_lowres nnUNetTrainerV2 Task523_NPC all --npz
-nnUNet_train 3d_lowres nnUNetTrainerV2 Task524_NPC all --npz
-nnUNet_train 3d_lowres nnUNetTrainerV2 Task525_NPC all --npz
-nnUNet_train 3d_lowres nnUNetTrainerV2 Task526_NPC all --npz
+# nnUNet_train 3d_lowres nnUNetTrainerV2 Task520_NPC all --npz
+# nnUNet_train 3d_lowres nnUNetTrainerV2 Task521_NPC all --npz
+# nnUNet_train 3d_lowres nnUNetTrainerV2 Task522_NPC all --npz
+# nnUNet_train 3d_lowres nnUNetTrainerV2 Task523_NPC all --npz
+# nnUNet_train 3d_lowres nnUNetTrainerV2 Task524_NPC all --npz
+# nnUNet_train 3d_lowres nnUNetTrainerV2 Task525_NPC all --npz
+# nnUNet_train 3d_lowres nnUNetTrainerV2 Task526_NPC all --npz
 
-# nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task520_NPC all --npz
-# nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task521_NPC all --npz
-# nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task522_NPC all --npz
-# nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task523_NPC all --npz
-# nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task524_NPC all --npz
-# nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task525_NPC all --npz
-# nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task526_NPC all --npz
+nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task520_NPC all 
+nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task521_NPC all
+nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task522_NPC all 
+nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task523_NPC all 
+nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task524_NPC all 
+nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task525_NPC all 
+nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task526_NPC all 
