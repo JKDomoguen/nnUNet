06bc5ac7da034004fc988731c22c07ed58072f55
Jansen Domoguen <jansenkeith.domoguen@gmail.com>
2022-10-03 08:50:06 +0800

    Adding NNUNet full cascade

--- run_train.sh
+++ run_train.sh
@@ -16,10 +16,19 @@
 # nnUNet_train 3d_lowres nnUNetTrainerV2 Task525_NPC all --npz
 # nnUNet_train 3d_lowres nnUNetTrainerV2 Task526_NPC all --npz
 
-nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task520_NPC all 
-nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task521_NPC all
-nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task522_NPC all 
-nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task523_NPC all 
-nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task524_NPC all 
-nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task525_NPC all 
-nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task526_NPC all 
+# nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task520_NPC all 
+# nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task521_NPC all
+# nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task522_NPC all 
+# nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task523_NPC all 
+# nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task524_NPC all 
+# nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task525_NPC all 
+# nnUNet_train 3d_cascade_fullres nnUNetTrainerV2CascadeFullRes Task526_NPC all 
+
+
+nnUNet_train 2d nnUNetTrainerV2 Task520_NPC all
+nnUNet_train 2d nnUNetTrainerV2 Task521_NPC all
+nnUNet_train 2d nnUNetTrainerV2 Task522_NPC all
+nnUNet_train 2d nnUNetTrainerV2 Task523_NPC all
+nnUNet_train 2d nnUNetTrainerV2 Task524_NPC all
+nnUNet_train 2d nnUNetTrainerV2 Task525_NPC all
+nnUNet_train 2d nnUNetTrainerV2 Task526_NPC all
